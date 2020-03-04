63795449035e7fb096b821ea3d17d083f100ea90
David Hernandez <davidhernandezruiz@gmail.com>
2020-03-04 12:59:40 +0100

    Increase version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="presidiopy", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="David Hernandez",
     author_email="hello@davidhernandez.info",
     description="An API library to interact with Microsoft Presidio",
