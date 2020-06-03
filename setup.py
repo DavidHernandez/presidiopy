253f5e32806f8a3b26954ba2fb8486ed0233a8a7
Gabriel Medina <gmedina197@gmail.com>
2020-06-03 18:30:58 -0300

    changing name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="presidiopy", # Replace with your own username
+    name="presidiopy2", # Replace with your own username
     version="0.0.2",
     author="David Hernandez",
     author_email="hello@davidhernandez.info",
